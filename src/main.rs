6f50ff6a12f688ee65c7f28af75e1bcfb38e140d
suzumenobu <idevtier@gmail.com>
2024-10-23 22:22:52 +0200

    refactor: Move telegram to RSS stuff into own module

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 mod config;
 mod repository;
 mod telegram;
+mod telegram2rss;
 
 use envconfig::Envconfig;
-use grammers_client::Client;
 use nanodb::nanodb::NanoDB;
-use rss::{Channel, ChannelBuilder};
 use serde::{Deserialize, Serialize};
-use std::fs::File;
-use std::io::BufReader;
-use std::path::Path;
 use std::time::Duration;
 
 #[derive(Deserialize, Serialize, Debug, Default)]
@@ -31,7 +27,7 @@ async fn main() -> anyhow::Result<()> {
     let db = NanoDB::open("db.json")?;
     let mut repository = repository::NanoDbTelegramChannelRepository::new(db);
 
-    watch_telegram_channel_updates(
+    telegram2rss::watch_updates(
         &client,
         &mut repository,
         &config.base_rss_feed_path,
@@ -42,105 +38,3 @@ async fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-async fn watch_telegram_channel_updates(
-    client: &Client,
-    repository: &mut impl repository::TelegramChannelRepository,
-    base_rss_feed_path: &Path,
-    update_interval: Duration,
-) -> anyhow::Result<()> {
-    loop {
-        update_rss_feeds_once(client, repository, base_rss_feed_path).await?;
-        tokio::time::sleep(update_interval).await;
-    }
-}
-
-async fn update_rss_feeds_once(
-    client: &Client,
-    repository: &mut impl repository::TelegramChannelRepository,
-    base_rss_feed_path: &Path,
-) -> anyhow::Result<()> {
-    let mut dialogs = client.iter_dialogs();
-    while let Some(dialog) = dialogs.next().await? {
-        let chat = dialog.chat();
-
-        match chat {
-            grammers_client::types::Chat::Channel(channel) => {
-                process_channel(&client, repository, channel, &base_rss_feed_path).await?;
-            }
-            _ => continue,
-        }
-    }
-    Ok(())
-}
-
-async fn process_channel(
-    client: &Client,
-    repository: &mut impl repository::TelegramChannelRepository,
-    channel: &grammers_client::types::chat::Channel,
-    base_rss_feed_path: &Path,
-) -> anyhow::Result<()> {
-    log::info!("{}", channel.title());
-
-    let mut channel_info = repository
-        .find_channel_info_by_id(&channel.id())
-        .await?
-        .unwrap_or(ChannelInfo {
-            last_processed_message_id: 0,
-            rss_feed_file_name: format!("{}.xml", channel.username().unwrap()),
-        });
-
-    let rss_feed_path = base_rss_feed_path.join(&channel_info.rss_feed_file_name);
-    let mut rss_channel = match File::open(&rss_feed_path) {
-        Ok(file) => Channel::read_from(BufReader::new(file))?,
-        Err(_) => ChannelBuilder::default()
-            .title(channel.title())
-            .link(format!("https://t.me/{}", channel.username().unwrap()))
-            .description("Not supported yet")
-            .build(),
-    };
-
-    let mut messages = client.iter_messages(channel);
-    let mut last_message_id = None;
-
-    if let Some(message) = messages.next().await? {
-        last_message_id = Some(message.id());
-    }
-
-    match last_message_id {
-        Some(id) => {
-            let messages_to_process =
-                std::cmp::min(id - channel_info.last_processed_message_id, 10);
-            log::info!("{} messages will be processed", messages_to_process);
-            messages = messages.limit(messages_to_process as usize);
-
-            let mut items = Vec::with_capacity(messages_to_process as usize);
-
-            while let Some(message) = messages.next().await? {
-                let item = rss::ItemBuilder::default()
-                    .title(message.id().to_string())
-                    .description(message.text().to_string())
-                    .link(format!(
-                        "https://t.me/{}/{}",
-                        channel.username().unwrap(),
-                        message.id()
-                    ))
-                    .build();
-                items.push(item);
-            }
-
-            items.extend_from_slice(rss_channel.items());
-            rss_channel.set_items(items);
-
-            let file = File::create(rss_feed_path)?;
-            rss_channel.write_to(file)?;
-            channel_info.last_processed_message_id = id;
-            repository
-                .update_channel_info(&channel.id(), &channel_info)
-                .await?;
-        }
-        None => {
-            println!("There is no any unprocessed messages");
-        }
-    }
-    Ok(())
-}
---
+++ src/telegram2rss.rs
@@ -0,0 +1,109 @@
+use std::{fs::File, io::BufReader, path::Path, time::Duration};
+
+use grammers_client::Client;
+use rss::{Channel, ChannelBuilder};
+
+use crate::{repository, ChannelInfo};
+
+pub async fn watch_updates(
+    client: &Client,
+    repository: &mut impl repository::TelegramChannelRepository,
+    base_rss_feed_path: &Path,
+    update_interval: Duration,
+) -> anyhow::Result<()> {
+    loop {
+        update_rss_feeds_once(client, repository, base_rss_feed_path).await?;
+        tokio::time::sleep(update_interval).await;
+    }
+}
+
+async fn update_rss_feeds_once(
+    client: &Client,
+    repository: &mut impl repository::TelegramChannelRepository,
+    base_rss_feed_path: &Path,
+) -> anyhow::Result<()> {
+    let mut dialogs = client.iter_dialogs();
+    while let Some(dialog) = dialogs.next().await? {
+        let chat = dialog.chat();
+
+        match chat {
+            grammers_client::types::Chat::Channel(channel) => {
+                process_channel(&client, repository, channel, &base_rss_feed_path).await?;
+            }
+            _ => continue,
+        }
+    }
+    Ok(())
+}
+
+async fn process_channel(
+    client: &Client,
+    repository: &mut impl repository::TelegramChannelRepository,
+    channel: &grammers_client::types::chat::Channel,
+    base_rss_feed_path: &Path,
+) -> anyhow::Result<()> {
+    log::info!("{}", channel.title());
+
+    let mut channel_info = repository
+        .find_channel_info_by_id(&channel.id())
+        .await?
+        .unwrap_or(ChannelInfo {
+            last_processed_message_id: 0,
+            rss_feed_file_name: format!("{}.xml", channel.username().unwrap()),
+        });
+
+    let rss_feed_path = base_rss_feed_path.join(&channel_info.rss_feed_file_name);
+    let mut rss_channel = match File::open(&rss_feed_path) {
+        Ok(file) => Channel::read_from(BufReader::new(file))?,
+        Err(_) => ChannelBuilder::default()
+            .title(channel.title())
+            .link(format!("https://t.me/{}", channel.username().unwrap()))
+            .description("Not supported yet")
+            .build(),
+    };
+
+    let mut messages = client.iter_messages(channel);
+    let mut last_message_id = None;
+
+    if let Some(message) = messages.next().await? {
+        last_message_id = Some(message.id());
+    }
+
+    match last_message_id {
+        Some(id) => {
+            let messages_to_process =
+                std::cmp::min(id - channel_info.last_processed_message_id, 10);
+            log::info!("{} messages will be processed", messages_to_process);
+            messages = messages.limit(messages_to_process as usize);
+
+            let mut items = Vec::with_capacity(messages_to_process as usize);
+
+            while let Some(message) = messages.next().await? {
+                let item = rss::ItemBuilder::default()
+                    .title(message.id().to_string())
+                    .description(message.text().to_string())
+                    .link(format!(
+                        "https://t.me/{}/{}",
+                        channel.username().unwrap(),
+                        message.id()
+                    ))
+                    .build();
+                items.push(item);
+            }
+
+            items.extend_from_slice(rss_channel.items());
+            rss_channel.set_items(items);
+
+            let file = File::create(rss_feed_path)?;
+            rss_channel.write_to(file)?;
+            channel_info.last_processed_message_id = id;
+            repository
+                .update_channel_info(&channel.id(), &channel_info)
+                .await?;
+        }
+        None => {
+            println!("There is no any unprocessed messages");
+        }
+    }
+    Ok(())
+}
